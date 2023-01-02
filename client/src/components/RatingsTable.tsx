4980e2ea2ab776799b1447406b1824912981de26
kaylarubin <kayla.rose12@yahoo.com>
2023-01-01 22:59:06 -0700

    implements scroll bar for overflow rows

--- client/src/components/RatingsTable.tsx
+++ client/src/components/RatingsTable.tsx
@@ -11,6 +11,12 @@ const users: User[] = [
   { id: 0, name: "lemon-cello", room: "kay", vote: 6 },
   { id: 0, name: "ChickenTendiesss", room: "kay", vote: 8 },
   { id: 0, name: "DopamineKilla", room: "kay", vote: 3 },
+  { id: 0, name: "kimmy", room: "kay", vote: 0 },
+  { id: 0, name: "kimbop", room: "kay", vote: 1 },
+  { id: 0, name: "curry", room: "kay", vote: 5 },
+  { id: 0, name: "lemon-cello", room: "kay", vote: 6 },
+  { id: 0, name: "ChickenTendiesss", room: "kay", vote: 8 },
+  { id: 0, name: "DopamineKilla", room: "kay", vote: 3 },
 ];
 
 const MAX_VOTE = 10;
@@ -23,24 +29,26 @@ interface Props {}
 
 export const RatingsTable: React.FC<Props> = (props) => {
   return (
-    <div className="RatingsTable__container">
-      {users.map((user) => {
-        return (
-          <div key={user.id} className="UserRow__row-container">
-            <img className="UserRow__icon" src={testIcon} />
-            <div className="RatingBar__container">
-              <div className="RatingBar__username">{user.name}</div>
-              <div className="RatingBar__outer-bar RatingBar__base-bar">
-                <div
-                  className="RatingBar__inner-bar RatingBar__base-bar"
-                  style={{ width: calculatePercentFromVote(user.vote) }}
-                ></div>
+    <div className="RatingsTable__first-container">
+      <div className="RatingsTable__second-container">
+        {users.map((user) => {
+          return (
+            <div key={user.id} className="UserRow__row-container">
+              <img className="UserRow__icon" src={testIcon} />
+              <div className="RatingBar__container">
+                <div className="RatingBar__username">{user.name}</div>
+                <div className="RatingBar__outer-bar RatingBar__base-bar">
+                  <div
+                    className="RatingBar__inner-bar RatingBar__base-bar"
+                    style={{ width: calculatePercentFromVote(user.vote) }}
+                  ></div>
+                </div>
+                <div className="RatingBar__score">{user.vote}</div>
               </div>
-              <div className="RatingBar__score">{user.vote}</div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
